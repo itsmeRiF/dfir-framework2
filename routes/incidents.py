b3f1a0d7df96ef57ce9db9a9dd44fa52662ec8b8
itsmeRiF <arif.ali.khan12@gmail.com>
2026-07-08 15:35:24 +0530

    Update incidents.py --showing incident with case ID
    well, enhancing the UI and use-case

--- routes/incidents.py
+++ routes/incidents.py
@@ -6,5 +6,15 @@ incident_bp = Blueprint("incident", __name__)
 
 @incident_bp.route("/incidents/<int:case_id>")
 def incidents(case_id):
-    data = Incident.query.filter_by(case_id=case_id).all()
-    return render_template("incidents.html", incidents=data)
+
+    data = (
+        Incident.query
+        .filter_by(case_id=case_id)
+        .all()
+    )
+
+    return render_template(
+        "incidents.html",
+        incidents=data,
+        case_id=case_id
+    )
