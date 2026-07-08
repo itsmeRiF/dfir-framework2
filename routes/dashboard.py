ab8929e73ea797d1e6e52f1320a08a5b5057f0aa
itsmeRiF <arif.ali.khan12@gmail.com>
2026-07-08 15:32:31 +0530

    Update dashboard.py
    changed the UI

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,10 +1,62 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+
+from database.db import db
+
+from models.case import Case
+from models.evidence import Evidence
+from models.event import Event
 
 dashboard_bp = Blueprint("dashboard", __name__)
 
 
 @dashboard_bp.route("/dashboard")
-@login_required
-def home():
-    return render_template("dashboard.html")
+def dashboard():
+
+    # Statistics
+    total_cases = Case.query.count()
+
+    evidence_count = Evidence.query.count()
+
+    event_count = Event.query.count()
+
+    critical_count = Event.query.filter_by(
+        severity="critical"
+    ).count()
+
+    # Latest Activity
+    recent_events = (
+        Event.query
+        .order_by(Event.timestamp.desc())
+        .limit(10)
+        .all()
+    )
+
+    recent_activity = []
+
+    for event in recent_events:
+
+        recent_activity.append({
+
+            "time": event.timestamp,
+
+            "description": f"{event.rule_title} ({event.computer})",
+
+            "status": event.severity.capitalize()
+
+        })
+
+    return render_template(
+
+        "dashboard.html",
+
+        total_cases=total_cases,
+
+        evidence_count=evidence_count,
+
+        event_count=event_count,
+
+        critical_count=critical_count,
+
+        recent_activity=recent_activity
+
+    )
