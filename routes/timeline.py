0fcc6e71ef7c82385c64c556adab54a48aacf967
itsmeRiF <arif.ali.khan12@gmail.com>
2026-07-08 15:36:26 +0530

    Update timeline.py --added visualisation through graphical timeline
    gives a birds-eye view of total events and the hotspots of crucial dates with most events

--- routes/timeline.py
+++ routes/timeline.py
@@ -1,14 +1,89 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
-
+from flask import Blueprint, render_template, request
 from models.event import Event
+from datetime import datetime
+import json
 
-timeline_bp = Blueprint("timeline", __name__)
+
+timeline_bp = Blueprint(
+    "timeline",
+    __name__
+)
+
 
 
 @timeline_bp.route("/timeline/<int:case_id>")
-@login_required
 def timeline(case_id):
-    events = Event.query.filter_by(case_id=case_id).all()
-    events = sorted(events, key=lambda x: str(x.timestamp))
-    return render_template("timeline.html", events=events, case_id=case_id)
+
+
+    start=request.args.get("start")
+
+    end=request.args.get("end")
+
+
+    query = Event.query.filter_by(
+        case_id=case_id
+    )
+
+
+    if start:
+
+        query=query.filter(
+            Event.timestamp >= datetime.fromisoformat(start)
+        )
+
+
+    if end:
+
+        query=query.filter(
+            Event.timestamp <= datetime.fromisoformat(end)
+        )
+
+
+    events=query.order_by(
+        Event.timestamp.asc()
+    ).all()
+
+
+
+    # Graph Data
+
+    freq={}
+
+
+    for e in events:
+
+        if not e.timestamp:
+            continue
+
+        day = e.timestamp.strftime("%Y-%m-%d")
+
+        freq[day] = freq.get(day,0) + 1
+
+    graph_labels=json.dumps(
+        list(freq.keys())
+    )
+
+
+    graph_values=json.dumps(
+        list(freq.values())
+    )
+
+
+
+    return render_template(
+
+        "timeline.html",
+
+        events=events,
+
+        case_id=case_id,
+
+        start=start or "",
+
+        end=end or "",
+
+        graph_labels=graph_labels,
+
+        graph_values=graph_values
+
+    )
